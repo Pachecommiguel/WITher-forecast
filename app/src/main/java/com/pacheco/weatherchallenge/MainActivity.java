365b6971742fced36644b0ad25f6f8f5031fe1d9
Miguel Matos <pacheco.mmiguel@gmail.com>
2021-04-01 18:51:15 +0100

    initial commit; empty project with one activity

---
+++ app/src/main/java/com/pacheco/weatherchallenge/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pacheco.weatherchallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
